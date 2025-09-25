f6339f9c137b5311fcf8e40516840b195fcf8ae4
f-radosta <168348485+f-radosta@users.noreply.github.com>
2025-09-25 13:16:15 +0300

    prida transpozici do prj01 mereni

--- demo/src/main/java/com/example/prj01_arrays_and_memory.java
+++ demo/src/main/java/com/example/prj01_arrays_and_memory.java
@@ -12,19 +12,31 @@ public class prj01_arrays_and_memory {
 
         // Měření: po řádcích
         long t0 = System.nanoTime();
-        sumRowMajor(a);
+        double sum = sumRowMajor(a);
         long t1 = System.nanoTime();
 
         // Měření: po sloupcích
         long t2 = System.nanoTime();
-        sumColMajor(a);
+        double sum2 = sumColMajor(a);
         long t3 = System.nanoTime();
+
+        // Měření: po řádcích po transpozici ze sloupců
+        long t4 = System.nanoTime();
+        double[][] b = doTransposition(a);
+        double sum3 = sumRowMajor(b);
+        long t5 = System.nanoTime();
 
         double rowTime = (t1 - t0) / 1_000_000_000.0;
         double colTime = (t3 - t2) / 1_000_000_000.0;
+        double transpositionTime = (t5 - t4) / 1_000_000_000.0;
+
+        System.out.printf("sum A: %f\n", sum);
+        System.out.printf("sum B: %f\n", sum2);
+        System.out.printf("sum C: %f\n", sum3);
 
         System.out.printf("Po řádcích: %.3fs\n", rowTime);
         System.out.printf("Po sloupcích: %.3fs\n", colTime);
+        System.out.printf("Po transpozici: %.3fs\n", transpositionTime);
     }
 
     private static double[][] allocateAndFill(int rows, int cols) {
@@ -63,6 +75,20 @@ public class prj01_arrays_and_memory {
             }
         }
         return sum;
+    }
+
+    // Provede transpozici matice
+    private static double[][] doTransposition(double[][] a) {
+        int rows = a.length;
+        int cols = a[0].length;
+        double[][] b = new double[cols][rows];
+        for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
+            double[] aRow = a[rowIndex];
+            for (int colIndex = 0; colIndex < cols; colIndex++) {
+                b[colIndex][rowIndex] = aRow[colIndex];
+            }
+        }
+        return b;
     }
 
 }
