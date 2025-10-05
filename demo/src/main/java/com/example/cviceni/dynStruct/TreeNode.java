9dacf1062e27b5209acca08cd4b208d8f580611d
f-radosta <168348485+f-radosta@users.noreply.github.com>
2025-10-05 17:40:27 +0300

    add cviceni dir

---
+++ demo/src/main/java/com/example/cviceni/dynStruct/TreeNode.java
@@ -0,0 +1,20 @@
+package com.example.cviceni.dynStruct;
+
+public class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeElem{" +
+                "data=" + data +
+                '}';
+    }
+}
